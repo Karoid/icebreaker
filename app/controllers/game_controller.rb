d8c0b3974dd1aaf9edd242a5a241d76999ddace4
Karoid <shj5508@naver.com>
2017-06-01 18:25:10 +0900

    can enter room

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -6,15 +6,25 @@ class GameController < WebsocketRails::BaseController
       send_message :connection_success, {message: 'web socket connection failed'}, :namespace => 'game'
     end
   end
-  
+
   def room_connect
-    
+
+    if message == '00000'
+      send_message :room_connect_status, {message: 'room connection success', status:true}, :namespace => 'game'
+    else
+      send_message :room_connect_status, {message: 'room connection failed', status:false}, :namespace => 'game'
+    end
+
+=begin
+    # 여기에 기존 코드를 작성해놓았습니다. 읽고 지워주세요
+    # params 처럼 받은 정보는 message에 저장됩니다.
     code = Room.find(roomcode)
-    
-    #if code != nil
-    #  send_message :room_connect_success, {message: 'room connection success'}, :namespace => 'game'
-    #else
-    #  send_message :room_connect_success, {message: 'room connection failed'}, :namespace => 'game'
-    #end
+
+    if code != nil
+      send_message :room_connect_success, {message: 'room connection success'}, :namespace => 'game'
+    else
+      send_message :room_connect_success, {message: 'room connection failed'}, :namespace => 'game'
+    end
+=end
   end
 end
