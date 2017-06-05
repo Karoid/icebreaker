ac580f8cbf80fbbf57d690d5a71321366c4a783a
David DoHyung Kim <davidkim9204@naver.com>
2017-06-05 12:15:56 +0000

    변수이름 수정

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,12 +6,12 @@ class HomeController < ApplicationController
 
     def create_room
        $r = Random.new.rand(0...9999)
-       create = Room.new
-       create.code = $r
-       create.save
+       room = Room.new
+       room.code = $r
+       room.save
        host = Player.new
        host.user_id = current_user.id
-       host.room_id = create.id
+       host.room_id = room.id
        host.point = 1
        host.save
        
@@ -22,10 +22,10 @@ class HomeController < ApplicationController
     def join_room
         room_id = Room.where(code: params[:room_code]).limit(1)[0].id
         unless Player.where(user_id: current_user.id, room_id: room_id).exists?
-           play = Player.new
-           play.user_id = current_user.id
-           play.room_id = room_id
-           play.save
+           player = Player.new
+           player.user_id = current_user.id
+           player.room_id = room_id
+           player.save
         end
        
        redirect_to '/home/game/'+$r.to_s
